4aaca081ce19b052581af271fc33d2c97d2050ad
Dorgan <sebastien.dorgan@gmail.com>
2017-10-22 13:28:32 +0200

    improve driver interface

--- drivers/driver.go
+++ drivers/driver.go
@@ -1,21 +1,28 @@
-package cloud
+package drivers
+
+type CloudObject struct{
+	Id string
+}
 
 type KeyPair struct {
-	Id              string
+	CloudObject
 	Name            string
 	Private_content []byte
 	Public_content  []byte
 }
 
-type VMSize struct {
-	Id    string
+type VMTemplate struct {
+	CloudObject
+	Name  string
 	Cores int
-	Ram   float32
-	Disk  float32
+	RamSize   float32
+	DiskSize  float32
+	CPUFrequency float32
+	Price float32
 }
 
 type VMState int
-/*
+
 const (
 	STOPPED  VMState = iota
 	STARTING
@@ -23,44 +30,74 @@ const (
 	STOPPING
 	IN_ERROR
 )
-*/
+var vmStates = [...]string {
+	"STOPPED",
+	"STARTING",
+	"STARTED",
+	"STOPPING",
+	"IN_ERROR",
+}
+
+func (state VMState) String() string {
+	return vmStates[state - 1]
+}
 
 type VM struct {
-	Id        string
+	CloudObject
 	Name      string
 	PrivateIp string
 	PublicIp  string
-	Size      VMSize
-	Status    VMState
+	Size      VMTemplate
+	State     VMState
 }
 type Volume struct {
-	Id   string
+	CloudObject
 	Name string
 	Size float32
+	Category string
 	Available bool
 }
 
 type VolumeAttachment struct {
-	Id     string
+	CloudObject
 	Name   string
 	Volume Volume
 	VM     VM
 }
 
-type driver interface {
+type Image struct {
+	CloudObject
+	Name string
+}
+
+type Network struct {
+	CloudObject
+	Name string
+	CIDR string
+}
+
+type Driver interface {
+	ListImages() ([]Image, error)
+	ListVMTemplates() ([]VMTemplate, error)
+
 	CreateKeyPair(name string) (KeyPair, error)
 	GetKeyPair(id string) (KeyPair, error)
 	ListKeyPairs() ([]KeyPair, error)
 	DeleteKeyPair(id string) error
 
-	CreateVM(name string, pair KeyPair, size VMSize, public_ip bool) (VM, error)
+	CreateNetwork(name string) (Network, error)
+	GetNetwork(id string) (Network, error)
+	ListNetwork() ([]Network, error)
+	DeleteNetwork(id string) error
+
+	CreateVM(name string, size VMTemplate, image Image, keyPair KeyPair, network Network, public_ip bool) (VM, error)
 	GetVM(id string) (VM, error)
 	ListVMs() ([]VM, error)
 	DeleteVM(id string) error
 	StopVM(id string) error
 	StartVM(id string) error
 
-	CreateVolume(name string, size float32) (Volume, error)
+	CreateVolume(name string, size float32, category string) (Volume, error)
 	GetVolume(id string) (Volume, error)
 	ListVolumes() ([]Volume, error)
 	DeleteVolume(id string) error
